d61c9186c8ba699884da438f46b6b612d020b2c0
qqwangxiaow <ywwangxiaow@qq.com>
2019-08-01 23:40:01 +0800

    array

---
+++ 475-Heaters.cpp
@@ -0,0 +1,31 @@
+/*计算与左边heater的距离，计算与右边heater的距离，取最小值，O(n) */
+class Solution {
+public:
+    int findRadius(vector<int>& houses, vector<int>& heaters) {
+        sort(houses.begin(),houses.end());
+        sort(heaters.begin(),heaters.end());
+        vector<int>res(houses.size(),INT_MAX);
+        for(int i=0,h=0;i<houses.size()&&h<heaters.size();)
+        {
+            if(houses[i]<=heaters[h])
+            {
+                res[i]=heaters[h]-houses[i];
+                i++;
+            }
+            else h++;
+            
+        }
+        for(int i=houses.size()-1,h=heaters.size()-1;i>=0&&h>=0;)
+        {
+            if(houses[i]>=heaters[h])
+            {
+                res[i]=min(res[i],houses[i]-heaters[h]);
+                i--;
+            }
+            else h--;
+            
+        }
+        return *max_element(res.begin(),res.end());
+        
+    }
+};
