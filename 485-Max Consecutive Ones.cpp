57655ce21aa866f4a1db3c3e55f9303a18f019f0
qqwangxiaow <ywwangxiaow@qq.com>
2019-08-04 15:32:52 +0800

    array

---
+++ 485-Max Consecutive Ones.cpp
@@ -0,0 +1,13 @@
+/* 直接法，注意max_cnt计算放在上方消除边界限制*/
+class Solution {
+public:
+       int findMaxConsecutiveOnes(vector<int>& nums) {
+        int max_cnt = 0, cnt = 0;
+        for (auto n : nums) {
+            if (n == 1) max_cnt = max(++cnt, max_cnt);
+            else cnt = 0;
+        }
+        return max_cnt;
+    }
+
+};
