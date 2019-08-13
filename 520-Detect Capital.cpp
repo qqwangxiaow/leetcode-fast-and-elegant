43979768d6337d91fdd1e1b77d54cf690438de91
qqwangxiaow <ywwangxiaow@qq.com>
2019-08-13 21:41:58 +0800

    idea

---
+++ 520-Detect Capital.cpp
@@ -0,0 +1,16 @@
+/*count*/
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int count=0;
+        for(int i=0;i<word.size();i++)
+        {
+            if(word[i]>='A'&&word[i]<='Z')
+            {
+                count++;
+            }
+        }
+        return (count==0||count==word.size()||(count==1&&word[0]>='A'&&word[0]<='Z'));
+        
+    }
+};
