f9bba0a0505da513ced543590e41cf5e29e77f46
qqwangxiaow <ywwangxiaow@qq.com>
2019-08-20 20:20:58 +0800

    tree

---
+++ 558-Quad Tree Intersection.cpp
@@ -0,0 +1,20 @@
+/*awesome*/
+class Solution {
+public:
+    Node* intersect(Node* quadTree1, Node* quadTree2) {
+        if(quadTree1->isLeaf) return quadTree1->val ? quadTree1 : quadTree2; 
+        if(quadTree2->isLeaf) return quadTree2->val ? quadTree2 : quadTree1;    
+        auto tl = intersect(quadTree1->topLeft, quadTree2->topLeft);
+        auto tr = intersect(quadTree1->topRight, quadTree2->topRight);
+        auto bl = intersect(quadTree1->bottomLeft, quadTree2->bottomLeft);
+        auto br = intersect(quadTree1->bottomRight, quadTree2->bottomRight);
+        bool val = tl->val;
+        if(tl->isLeaf && tr->isLeaf && bl->isLeaf && br->isLeaf &&
+          val == tr->val && val == bl->val && val == br->val){
+            delete tl; delete tr;
+            delete bl; delete br;
+            return new Node(val, true, nullptr, nullptr, nullptr, nullptr);
+        }
+        return new Node(false, false, tl, tr, bl, br);          
+    }
+};
