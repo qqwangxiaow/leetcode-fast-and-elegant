3c8c8134f7c024e5d3d1d186c46bf7475dffd900
qqwangxiaow <ywwangxiaow@qq.com>
2019-08-23 23:53:19 +0800

    tree

---
+++ 590-N-ary Tree Postorder Traversal.cpp
@@ -0,0 +1,22 @@
+/*stack*/
+class Solution {
+public:
+    vector<int> postorder(Node* root) {
+     if(!root)
+         return {};
+        vector<int>res;
+        stack<Node*>s;
+        s.push(root);
+        while(!s.empty())
+        {
+            Node *temp=s.top();
+            s.pop();
+            for(int i=0;i<temp->children.size();i++)
+                s.push(temp->children[i]);
+            res.push_back(temp->val);
+        }
+        reverse(res.begin(),res.end());
+        return res;
+        
+    }
+};
