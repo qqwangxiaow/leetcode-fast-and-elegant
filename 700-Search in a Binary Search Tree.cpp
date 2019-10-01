c53e362b91c135bc961cd87a6154caf3a5b2421e
qqwangxiaow <ywwangxiaow@qq.com>
2019-10-01 20:40:21 +0800

    tree

---
+++ 700-Search in a Binary Search Tree.cpp
@@ -0,0 +1,7 @@
+//recrusive
+TreeNode* searchBST(TreeNode* root, int val) {
+    while (root != nullptr && root->val != val) {
+      root = (root->val > val) ? root->left : root->right;
+    }
+    return root;
+}
