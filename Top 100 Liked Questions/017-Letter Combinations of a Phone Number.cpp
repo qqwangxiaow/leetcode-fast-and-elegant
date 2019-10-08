9a56f0069d93c9d712e8b85d324424b820239c80
qqwangxiaow <ywwangxiaow@qq.com>
2019-10-08 10:52:12 +0800

    backtrack

--- Top 100 Liked Questions/017-Letter Combinations of a Phone Number.cpp
+++ Top 100 Liked Questions/017-Letter Combinations of a Phone Number.cpp
@@ -26,3 +26,45 @@ public:
         return ret;
     }
 };
+//backtrack
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        if(digits=="") return {};
+        vector<string>ret;
+        unordered_map<char,string>hash;
+        hash['2']="abc";
+        hash['3']="def";
+        hash['4']="ghi";
+        hash['5']="jkl";
+        hash['6']="mno";
+        hash['7']="pqrs";
+        hash['8']="tuv";
+        hash['9']="wxyz";
+        vector<string>pool;
+        for(char c:digits)
+        {
+            pool.push_back(hash[c]);
+        }
+        hash.clear();
+        string local="";
+        BackTracking(digits,ret,pool,local);
+        return ret;
+       
+    }
+    void BackTracking(string &digits,vector<string>&ret,vector<string>&pool,string &local)
+    {
+        if(local.size()==digits.size())
+        {
+            ret.push_back(local);
+            return;
+        }
+        string searchSpace=pool[local.size()];
+        for(char s:searchSpace)
+        {
+            local.push_back(s);
+            BackTracking(digits,ret,pool,local);
+            local.pop_back();
+        }
+    }
+};
