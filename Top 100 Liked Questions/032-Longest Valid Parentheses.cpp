4a76a582f409f5e8618015bc3e4735cee2d31636
qqwangxiaow <ywwangxiaow@qq.com>
2019-10-11 16:13:11 +0800

    dp

--- Top 100 Liked Questions/032-Longest Valid Parentheses.cpp
+++ Top 100 Liked Questions/032-Longest Valid Parentheses.cpp
@@ -79,3 +79,58 @@ public:
       
     }
 };
+
+//dp
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        if(s.size()<=1) return 0;
+        int curmax=0;
+        int n=s.size();
+        vector<int>dp(n,0);
+        for(int i=1;i<n;i++)
+        {
+            
+            if(s[i]==')')
+            {
+                if(s[i-1]=='(')
+                {
+                    dp[i]=(i-2)>=0?(dp[i-2]+2):2;
+                    curmax=max(dp[i],curmax);
+                }
+                else 
+                {
+                    if(i-dp[i-1]-1>=0&&s[i-dp[i-1]-1]=='(')
+                    {
+                        dp[i]=dp[i-1]+2+((i-dp[i-1]-2)>=0?dp[i-dp[i-1]-2]:0);
+                        curmax=max(dp[i],curmax);
+                    }
+                    
+                }
+            }
+            
+        }
+        return curmax;
+    }
+};
+//more concise
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        if(s.size()<=1) return 0;
+        int curmax=0;
+        int n=s.size();
+        vector<int>dp(n,0);
+        for(int i=1;i<n;i++)
+        {
+            
+            if(s[i]==')'&&i-dp[i-1]-1>=0&&s[i-dp[i-1]-1]=='(')
+            {
+                dp[i]=dp[i-1]+2+((i-dp[i-1]-2)>=0?dp[i-dp[i-1]-2]:0);
+                curmax=max(dp[i],curmax);
+            }
+            
+        }
+        return curmax;
+    }
+};
