c0b26b8f830a828586e4ab63309735a34e61f038
qqwangxiaow <ywwangxiaow@qq.com>
2019-10-18 11:11:39 +0800

    backtrack

---
+++ Top 100 Liked Questions/046-Permutations.cpp
@@ -0,0 +1,27 @@
+//backtrack
+//if & need popback else not needed
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>ret;
+        helper(ret,nums,0);
+        return ret;
+    }
+    void helper(vector<vector<int>>&ret,vector<int>nums,int start)
+    {
+        if(start==nums.size())
+        {
+            ret.push_back(nums);
+        }
+        else
+        {
+            for(int i=start;i<nums.size();i++)
+            {
+                swap(nums[i],nums[start]);
+                helper(ret,nums,start+1);   
+               // swap(nums[i],nums[start]);
+            }
+        }
+        
+    }
+};
