d7e5e7b0d75676df17ba9325dc64e34cf5952109
qqwangxiaow <ywwangxiaow@qq.com>
2019-10-23 23:03:22 +0800

    math

--- 812-Largest Triangle Area.cpp
+++ 812-Largest Triangle Area.cpp
@@ -10,3 +10,30 @@ public:
         return res;
     }
 };
+
+//海伦公式
+class Solution {
+public:
+    double largestTriangleArea(vector<vector<int>>& points) {
+        double res=0;
+        for(auto &i:points)
+            for(auto &j:points)
+                for(auto &k:points)
+                    res=max(res,getarea(i,j,k));
+        return res;
+    }
+    double getarea(vector<int>&a,vector<int>&b,vector<int>&c)
+    {
+        double dis1=getdis(a,b);
+        double dis2=getdis(a,c);
+        double dis3=getdis(b,c);
+        double s=(dis1+dis2+dis3)/2;
+        double area=sqrt(s*(s-dis1)*(s-dis2)*(s-dis3));
+        return area;
+        
+    }
+    double getdis(vector<int>&a,vector<int>&b)
+    {
+        return sqrt((a[0]-b[0])*(a[0]-b[0])+(a[1]-b[1])*(a[1]-b[1]));
+    }
+};
