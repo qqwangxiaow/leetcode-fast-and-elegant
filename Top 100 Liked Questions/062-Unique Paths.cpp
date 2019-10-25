fc1821a54840ced8a7ac5304d5124dcae3a26acd
qqwangxiaow <ywwangxiaow@qq.com>
2019-10-25 11:49:31 +0800

    dp

---
+++ Top 100 Liked Questions/062-Unique Paths.cpp
@@ -0,0 +1,32 @@
+//dp
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>dp(n,vector<int>(m,1));
+        for(int i=1;i<n;i++)
+        {
+            for(int j=1;j<m;j++)
+            { 
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return dp[n-1][m-1];
+        
+    }
+};
+//optimize
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<int>dp(m,1);
+        for(int i=1;i<n;i++)
+        {
+            for(int j=1;j<m;j++)
+            { 
+                dp[j]+=dp[j-1];
+            }
+        }
+        return dp[m-1];
+        
+    }
+};
