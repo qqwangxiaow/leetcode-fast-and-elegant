cedc92e8068283c1d9516abe02f0a8abfc41b667
qqwangxiaow <ywwangxiaow@qq.com>
2019-11-25 13:14:27 +0800

    tree

--- Top 100 Liked Questions/101. Symmetric Tree.cpp
+++ Top 100 Liked Questions/101. Symmetric Tree.cpp
@@ -1,4 +1,4 @@
-//recrusive
+//recursive
 class Solution {
 public:
     bool isSymmetric(TreeNode* root) {
@@ -18,3 +18,34 @@ public:
     }
 };
 
+//iterative using two queue
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        TreeNode *t1,*t2;
+        if(!root)
+            return true;
+        queue<TreeNode *>q1,q2;
+        q1.push(root->left);
+        q2.push(root->right);
+        while(!q1.empty()&&!q2.empty())
+        {
+            t1=q1.front();
+            q1.pop();
+            t2=q2.front();
+            q2.pop();
+            if(!t1&&!t2)
+                continue;
+            if(!t1||!t2)
+                return false;
+            if(t1->val!=t2->val)
+                return false;
+            q1.push(t1->left);
+            q1.push(t1->right);
+            q2.push(t2->right);
+            q2.push(t2->left);
+        }
+        return true;
+    }
+    
+};
