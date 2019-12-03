531066d4974de100616ff95f8680ed6adb3ae85f
qqwangxiaow <ywwangxiaow@qq.com>
2019-12-03 14:51:49 +0800

    bit

---
+++ Top 100 Liked Questions/136-Single Number.cpp
@@ -0,0 +1,12 @@
+//XOR
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ret=nums[0];
+        for(int i=1;i<nums.size();i++)
+        {
+            ret^=nums[i];
+        }
+        return ret; 
+    }
+};
