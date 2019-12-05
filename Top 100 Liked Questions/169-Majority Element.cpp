3bf3d2f456cd1d30f3be3d866b7dbbc4e404cc6e
qqwangxiaow <ywwangxiaow@qq.com>
2019-12-05 13:52:44 +0800

    array

---
+++ Top 100 Liked Questions/169-Majority Element.cpp
@@ -0,0 +1,26 @@
+//moore's voting
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int ret=nums[0];
+        int count=1;
+        for(int i=1;i<nums.size();i++)
+        {
+            if(nums[i]==ret)
+            {
+                count++;
+            }
+            else
+            {
+                count--;
+                if(count<=0)
+                {
+                    ret=nums[i];
+                    count=1;
+                }
+            }
+        }
+        return ret;
+        
+    }
+};
