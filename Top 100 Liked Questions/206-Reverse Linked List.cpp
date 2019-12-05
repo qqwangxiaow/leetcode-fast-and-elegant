e28f1cfe670f99649b48cc47fd8894bc3ffcafda
qqwangxiaow <ywwangxiaow@qq.com>
2019-12-05 15:41:19 +0800

    list

--- Top 100 Liked Questions/206-Reverse Linked List.cpp
+++ Top 100 Liked Questions/206-Reverse Linked List.cpp
@@ -15,3 +15,16 @@ public:
         return pre;
     }
 };
+//recrusive
+//key :head->next->next=head;
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(!head||!head->next)
+            return head;
+        ListNode *temp=reverseList(head->next);
+        head->next->next=head;
+        head->next=NULL;
+        return temp;
+    }
+};
