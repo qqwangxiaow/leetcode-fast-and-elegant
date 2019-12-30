54637f1b32e868eba98052b4996c3c028805d946
qqwangxiaow <ywwangxiaow@qq.com>
2019-12-30 21:40:51 +0800

    dp

--- Top Interview Questions/010-Regular Expression Matching.cpp
+++ Top Interview Questions/010-Regular Expression Matching.cpp
@@ -21,3 +21,62 @@ public:
         
     }
 };
+
+//two vector
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int m=s.size(),n=p.size();
+        vector<vector<int>>dp(m+1,vector<int>(n+1));
+        vector<int>pre(n+1,0);
+        vector<int>cur(n+1,0);
+        cur[0]=1;
+        for(int i=0;i<=m;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(p[j-1]=='*')
+                {
+                    cur[j]=cur[j-2]||(i&&pre[j]&&(s[i-1]==p[j-2]||p[j-2]=='.'));
+                }
+                else
+                {
+                    cur[j]=i&&pre[j-1]&&(s[i-1]==p[j-1]||(p[j-1]=='.'));
+                }  
+            }
+            fill(pre.begin(),pre.end(),0);
+            swap(pre,cur);
+        }
+        return pre[n];
+        
+    }
+};
+
+//dp[i-1][j-1]=pre
+//init pre=cur[0] only dp[0][0]=1
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int m=s.size(),n=p.size();
+        vector<int>cur(n+1,0);
+        for(int i=0;i<=m;i++)
+        {
+            int pre=cur[0];
+            cur[0]=!i;
+            for(int j=1;j<=n;j++)
+            {
+                int temp=cur[j];
+                if(p[j-1]=='*')
+                {
+                    cur[j]=cur[j-2]||(i&&cur[j]&&(s[i-1]==p[j-2]||p[j-2]=='.'));
+                }
+                else 
+                {
+                    cur[j]=i&&pre&&(s[i-1]==p[j-1]||p[j-1]=='.');
+                }
+                pre=temp;
+            }
+        }
+        return cur[n];
+    }
+};
