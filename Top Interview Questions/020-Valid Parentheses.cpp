f922b8b8efe628a916f7441964975615429fb32f
qqwangxiaow <ywwangxiaow@qq.com>
2020-01-01 20:53:55 +0800

    stack

---
+++ Top Interview Questions/020-Valid Parentheses.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>stk;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='('||s[i]=='['||s[i]=='{')
+                stk.push(s[i]);
+            else if(s[i]==')')
+            {
+                if(stk.empty()||stk.top()!='(')
+                    return false;
+                else
+                    stk.pop();
+            }
+            else if(s[i]==']')
+            {
+                if(stk.empty()||stk.top()!='[')
+                    return false;
+                else
+                    stk.pop();
+            }
+            else if(s[i]=='}')
+            {
+                if(stk.empty()||stk.top()!='{')
+                    return false;
+                else
+                    stk.pop();
+            }
+                
+        }
+        return stk.empty();
+        
+    }
+};
