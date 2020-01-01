cda52fc7fa4024feff88c4f5367da8026bd56ac4
qqwangxiaow <ywwangxiaow@qq.com>
2020-01-01 21:02:04 +0800

    list

---
+++ Top Interview Questions/021-Merge Two Sorted Lists.cpp
@@ -0,0 +1,26 @@
+//do not forget head=head->next 
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode dummy(0);
+        ListNode *head=&dummy;
+        while(l1&&l2)
+        {
+            if(l1->val<l2->val)
+            {
+                head->next=l1;
+                l1=l1->next;
+            }
+            else
+            {
+                head->next=l2;
+                l2=l2->next;
+            }
+            head=head->next;
+                
+        }
+        head->next=l1?l1:l2;
+        return dummy.next;
+        
+    }
+};
