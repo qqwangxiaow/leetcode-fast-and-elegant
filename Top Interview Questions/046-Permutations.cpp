43557f21e5a5abf5f7854488ccf472f57109670e
qqwangxiaow <ywwangxiaow@qq.com>
2020-01-09 14:55:08 +0800

    backtrack

---
+++ Top Interview Questions/046-Permutations.cpp
@@ -0,0 +1,26 @@
+//swap(start,i)
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>ret;
+        helper(ret,nums,0);
+        return ret;
+    }
+    void helper(vector<vector<int>>&ret,vector<int>&nums,int start)
+    {
+        if(start==nums.size())
+        {
+            ret.push_back(nums);
+            return;
+        }
+        for(int i=start;i<nums.size();i++)
+        {
+         
+            swap(nums[i],nums[start]);
+            helper(ret,nums,start+1);
+            swap(nums[i],nums[start]);
+            
+        }
+        
+    }
+};
