58aff758e8f862be5c7696355442fa646336dc90
qqwangxiaow <ywwangxiaow@qq.com>
2020-01-16 09:14:53 +0800

    mergesort

---
+++ Top Interview Questions/088-Merge Sorted Array.cpp
@@ -0,0 +1,17 @@
+//mergesort from right
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int len=m+n-1;
+        m--,n--;
+        while(m>=0&&n>=0)
+        {
+            nums1[len--]=nums1[m]>nums2[n]?nums1[m--]:nums2[n--];
+        }
+        while(n>=0)
+        {
+            nums1[len--]=nums2[n--];
+        }
+        
+    }
+};
