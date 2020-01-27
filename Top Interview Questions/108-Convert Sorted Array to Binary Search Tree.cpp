8c03679f8902fcdc9164a912fdfd130efa37f0bd
qqwangxiaow <ywwangxiaow@qq.com>
2020-01-27 13:01:47 +0800

    binary search

---
+++ Top Interview Questions/108-Convert Sorted Array to Binary Search Tree.cpp
@@ -0,0 +1,18 @@
+//find mid
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        int l=0,r=nums.size()-1;
+        return helper(nums,l,r);
+    }
+    TreeNode* helper(vector<int>&nums,int l,int r)
+    {
+        if(l>r)
+            return nullptr;
+        int m=l+(r-l)/2;
+        TreeNode* cur=new TreeNode(nums[m]);
+        cur->left=helper(nums,l,m-1);
+        cur->right=helper(nums,m+1,r);
+        return cur;
+    }
+};
