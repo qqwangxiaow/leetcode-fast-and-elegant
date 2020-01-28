e7e3a888869cab5dbdb1db90390f03540f5a20d4
qqwangxiaow <ywwangxiaow@qq.com>
2020-01-28 18:55:26 +0800

    dp

--- Top Interview Questions/122-Best Time to Buy and Sell Stock II.cpp
+++ Top Interview Questions/122-Best Time to Buy and Sell Stock II.cpp
@@ -1,3 +1,20 @@
+//真dp i表示天数 k表示交易次数 0表示没有持有股票 1表示持有股票
+//dp[i][k][0] = max(dp[i-1][k][0], dp[i-1][k][1] + prices[i])
+//dp[i][k][1] = max(dp[i-1][k][1], dp[i-1][k-1][0] - prices[i])
+//k不用管,i用迭代的方法
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int dp_0=0,dp_1=INT_MIN;
+        for(int i=0;i<prices.size();i++)
+        {
+            int temp=dp_0;
+            dp_0=max(dp_0,dp_1+prices[i]);
+            dp_1=max(dp_1,temp-prices[i]);
+        }
+        return dp_0;
+    }
+};
 //其实很简单 累加每一段涨的曲线 因为没限制交易次数
 class Solution {
 public:
