213ac7a0c6d1289d323caed9c693db5e86dd0c66
qqwangxiaow <ywwangxiaow@qq.com>
2020-02-01 18:23:59 +0800

    dp

---
+++ Top Interview Questions/198-House Robber.cpp
@@ -0,0 +1,15 @@
+//dp
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        vector<int>dp(nums.size()+1,0);
+        for(int i=1;i<=nums.size();i++)
+        {
+            if(i==1)
+                dp[i]=nums[i-1];
+            else
+                dp[i]=max(dp[i-1],dp[i-2]+nums[i-1]);
+        }
+        return dp[nums.size()];
+    }
+};
