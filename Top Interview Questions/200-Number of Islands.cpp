e006b3044df402557458612600cd64c11a347e8c
qqwangxiaow <ywwangxiaow@qq.com>
2020-02-01 18:37:24 +0800

    dfs

---
+++ Top Interview Questions/200-Number of Islands.cpp
@@ -0,0 +1,33 @@
+//dfs
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int ret=0;
+        for(int i=0;i<grid.size();i++)
+        {
+            for(int j=0;j<grid[0].size();j++)
+            {
+                if(grid[i][j]=='1')
+                {
+                    dfs(grid,i,j);
+                    ret++;
+                }
+            }
+        }
+        return ret;  
+    }
+    void dfs(vector<vector<char>>&grid,int i,int j)
+    {
+        if(i<0||j<0||i>=grid.size()||j>=grid[0].size())
+            return;
+        if(grid[i][j]=='1')
+        {
+            grid[i][j]='#';
+            dfs(grid,i-1,j);
+            dfs(grid,i+1,j);
+            dfs(grid,i,j-1);
+            dfs(grid,i,j+1);
+        }
+        
+    }
+};
