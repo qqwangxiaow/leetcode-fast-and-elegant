668d892a0f386ea85762db7b40e2b2da7f669034
qqwangxiaow <ywwangxiaow@qq.com>
2020-02-05 21:26:28 +0800

    easy

---
+++ Top Interview Questions/326-power of three.cpp
@@ -0,0 +1,16 @@
+//mod
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n==0)
+            return false;
+        while(n!=1)
+        {
+            if(n%3==0)
+                n/=3;
+            else
+                break;
+        }
+        return n==1;
+    }
+};
