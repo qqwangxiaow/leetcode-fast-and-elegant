40f054edecb1323dd25c5293d982432fecc396df
qqwangxiaow <ywwangxiaow@qq.com>
2020-02-06 22:58:58 +0800

    shuffle

---
+++ Top Interview Questions/384-Shuffle an Array.cpp
@@ -0,0 +1,27 @@
+//Fisher-Yates shuffle algorithm
+//swap nums[i] with random nums[i~len]
+class Solution {
+public:
+    Solution(vector<int>& nums) {
+        init=nums;
+        cur=nums;
+    }
+    /** Resets the array to its original configuration and return it. */
+    vector<int> reset() {
+        cur=init;
+        return cur;   
+    }
+    /** Returns a random shuffling of the array. */
+    vector<int> shuffle() {
+        int len=cur.size();
+        for(int i=0;i<len;i++)
+        {
+            int temp=rand()%(len-i)+i;
+            swap(cur[i],cur[temp]);
+        }
+        return cur;  
+    }
+private:
+    vector<int>init;
+    vector<int>cur;
+};
