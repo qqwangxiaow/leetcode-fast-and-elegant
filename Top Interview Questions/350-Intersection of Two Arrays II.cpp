b747290458177cdbd94217361a5367b1cb178010
qqwangxiaow <ywwangxiaow@qq.com>
2020-02-06 21:13:14 +0800

    map

---
+++ Top Interview Questions/350-Intersection of Two Arrays II.cpp
@@ -0,0 +1,22 @@
+//sort or map
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        int i=0,j=0,k=0;
+        while(i<nums1.size()&&j<nums2.size())
+        {
+            if(nums1[i]<nums2[j])
+                i++;
+            else if(nums1[i]>nums2[j])
+                j++;
+            else
+            {
+                nums1[k++]=nums1[i++];
+                j++;
+            }
+        }
+        return vector<int>(nums1.begin(),nums1.begin()+k);
+    }
+};
