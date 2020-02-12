9c6baf84b2f78aa684a946933c501e02930d8adb
qqwangxiaow <ywwangxiaow@qq.com>
2020-02-12 17:41:09 +0800

    stack

--- Top 100 Liked Questions/739-Daily Temperatures.cpp
+++ Top 100 Liked Questions/739-Daily Temperatures.cpp
@@ -1,3 +1,21 @@
+//using decrease stack
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& T) {
+        stack<int>s;
+        vector<int>ret(T.size(),0);
+        for(int i=0;i<T.size();i++)
+        {
+            while(!s.empty()&&T[s.top()]<T[i])
+            {
+                ret[s.top()]=i-s.top();
+                s.pop();
+            }
+            s.push(i);
+        }
+        return ret;
+    }
+};
 //brute force optimize solution
 class Solution {
 public:
