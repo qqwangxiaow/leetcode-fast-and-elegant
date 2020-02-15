53f42257b73ad920f9fbec1b563855c063daee96
qqwangxiaow <ywwangxiaow@qq.com>
2020-02-15 23:37:03 +0800

    list

---
+++ 剑指offer/面试题06-从尾到头打印链表.cpp
@@ -0,0 +1,15 @@
+//easy
+class Solution {
+public:
+    vector<int> reversePrint(ListNode* head) {
+        vector<int>ret;
+        while(head)
+        {
+            ret.push_back(head->val);
+            head=head->next;
+        }
+        reverse(ret.begin(),ret.end());
+        return ret;
+        
+    }
+};
