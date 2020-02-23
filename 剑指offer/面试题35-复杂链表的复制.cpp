8224291ae0c0000f04e40d4bd3db5d6522c1773e
qqwangxiaow <ywwangxiaow@qq.com>
2020-02-23 23:08:12 +0800

    list

---
+++ 剑指offer/面试题35-复杂链表的复制.cpp
@@ -0,0 +1,49 @@
+//1.cur->random?cur->random->next:nullptr
+//2.cur2->next=cur?cur->next:nullptr;
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if(!head)
+            return nullptr;
+        Node *cur=head;
+        while(cur)
+        {
+            Node *copy=new Node(cur->val);
+            copy->next=cur->next;
+            cur->next=copy;
+            cur=copy->next;
+        }
+        cur=head;
+        while(cur)
+        {
+            cur->next->random=cur->random?cur->random->next:nullptr;
+            cur=cur->next->next;
+        }
+        cur=head;
+        Node *head2=cur->next,*cur2=cur->next;
+        while(cur)
+        {
+            cur->next=cur2->next;
+            cur=cur->next;
+            cur2->next=cur?cur->next:nullptr;
+            cur2=cur2->next;
+        }
+        return head2;
+
+    }
+};
