b86f4a3b8e1e1833a0383e06ec864ab162e13c45
qqwangxiaow <ywwangxiaow@qq.com>
2020-03-04 09:26:34 +0800

    array

---
+++ 剑指offer/面试题57-和为s的两个数字.cpp
@@ -0,0 +1,23 @@
+//sorted
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int l=0,r=nums.size()-1;
+        while(l<r)
+        {
+            if(nums[l]+nums[r]==target)
+            {
+                return {nums[l],nums[r]};
+            }
+            else if(nums[l]+nums[r]<target)
+            {
+                l++;
+            }
+            else
+            {
+                r--;
+            }
+        }
+        return {};
+    }
+};
