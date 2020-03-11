8e70e53970255c299a3694063fe95fbb9bc07acb
qqwangxiaow <ywwangxiaow@qq.com>
2020-03-11 10:20:48 +0800

    recursive

---
+++ 112-Path Sum.cpp
@@ -0,0 +1,11 @@
+//[] 0 is false
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int sum) {
+        if(!root)
+            return false;
+        if(!root->left&&!root->right&&root->val==sum)
+            return true;
+        return hasPathSum(root->left,sum-root->val)||hasPathSum(root->right,sum-root->val);
+    }
+};
