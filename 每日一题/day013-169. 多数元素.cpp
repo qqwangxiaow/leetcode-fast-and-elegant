86ad90e99036d2df3336c7104618309c24587a68
qqwangxiaow <ywwangxiaow@qq.com>
2020-03-13 20:41:55 +0800

    moore vote

---
+++ 每日一题/day013-169. 多数元素.cpp
@@ -0,0 +1,26 @@
+//dont't forget initialization count=1 when exchange
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int ret=nums[0];
+        int count=1;
+        for(int i=1;i<nums.size();i++)
+        {
+            if(nums[i]==ret)
+            {
+                count++;
+            }
+            else
+            {
+                count--;
+                if(count==0)
+                {
+                    ret=nums[i];
+                    count=1;
+                }    
+            }
+        }
+        return ret;
+
+    }
+};
