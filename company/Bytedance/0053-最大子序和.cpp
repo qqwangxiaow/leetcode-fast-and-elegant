4f70f79690d4d11bfb0fe8fd3d63f9df773d0c8e
qqwangxiaow <ywwangxiaow@qq.com>
2020-03-14 09:27:59 +0800

    dp

---
+++ company/Bytedance/0053-最大子序和.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        if(nums.empty())
+            return -1;
+        vector<int>dp(nums.size(),0);
+        dp[0]=nums[0];
+        int ret=nums[0];
+        for(int i=1;i<nums.size();i++)
+        {
+            dp[i]=max(nums[i],dp[i-1]+nums[i]);
+        }
+        return *max_element(dp.begin(),dp.end());
+    }
+};
