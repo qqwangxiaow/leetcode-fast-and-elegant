b1ba2f38bafbc318dacd8e68cf1c7cbd2bb03c5e
qqwangxiaow <ywwangxiaow@qq.com>
2020-03-16 21:31:56 +0800

    dp

---
+++ company/Bytedance/0070-爬楼梯.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n<=1)
+            return 1;
+        if(n==2)
+            return 2;
+        int pre1=1,pre2=2;
+        int ret=0;
+        for(int i=3;i<=n;i++)
+        {
+            ret=pre1+pre2;
+            pre1=pre2;
+            pre2=ret;
+        }
+        return ret;
+    }
+};
