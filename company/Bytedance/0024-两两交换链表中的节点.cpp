2aa7da739fcf9dc9714ed91da82babf182e82b9c
qqwangxiaow <ywwangxiaow@qq.com>
2020-03-22 23:59:50 +0800

    recursive

---
+++ company/Bytedance/0024-两两交换链表中的节点.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(!head||!head->next)
+            return head;
+        ListNode *temp=swapPairs(head->next->next);
+        ListNode *cur=head->next;
+        cur->next=head;
+        head->next=temp;
+        return cur;
+    }
+};
