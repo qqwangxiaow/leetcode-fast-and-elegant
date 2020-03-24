285dfd72f0ba0f65bcfd10b18cb8df40a8f232de
qqwangxiaow <ywwangxiaow@qq.com>
2020-03-24 12:25:19 +0800

    gcd

---
+++ 每日一题/day025-365. 水壶问题.cpp
@@ -0,0 +1,11 @@
+//贝祖定理
+class Solution {
+public:
+    bool canMeasureWater(int x, int y, int z) {
+        if(x+y<z)  
+            return false;
+        if(x==0||y==0)
+            return z==0||(x+y)==z;
+        return z%__gcd(x,y)==0;
+    }
+};
