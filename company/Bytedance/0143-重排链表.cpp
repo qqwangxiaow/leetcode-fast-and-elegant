10c4bcdc664fd2791ba9dfb72b6a7b1b3b46d236
qqwangxiaow <ywwangxiaow@qq.com>
2020-03-28 20:15:53 +0800

    two pointers

---
+++ company/Bytedance/0143-重排链表.cpp
@@ -0,0 +1,37 @@
+//two pointers for split
+//reverse
+//merge
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        ListNode *slow=head,*fast=head;
+        if(!head||!head->next)
+            return;
+        while(fast->next&&fast->next->next)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        ListNode *newhead=slow->next;
+        slow->next=nullptr;
+        ListNode *pre=nullptr;
+        while(newhead)
+        {
+            ListNode *temp=newhead->next;
+            newhead->next=pre;
+            pre=newhead;
+            newhead=temp;
+        }
+        newhead=pre;
+        while(head&&newhead)
+        {
+            ListNode *temp=head->next;
+            ListNode *tempnew=newhead->next;
+            head->next=newhead;
+            newhead->next=temp;
+            head=temp;
+            newhead=tempnew;
+        }
+
+    }
+};
