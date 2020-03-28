45db8ce78636be715e2c33b79c2eb4aaca871194
qqwangxiaow <ywwangxiaow@qq.com>
2020-03-28 19:44:45 +0800

    gcd

---
+++ 每日一题/day027-914. 卡牌分组.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool hasGroupsSizeX(vector<int>& deck) {
+        unordered_map<int,int>mymap;
+        for(int i:deck)
+            mymap[i]++;
+        int res=0;
+        for(auto each:mymap)
+        {
+            res=__gcd(each.second,res);
+        }
+        return res>1;
+    }
+};
