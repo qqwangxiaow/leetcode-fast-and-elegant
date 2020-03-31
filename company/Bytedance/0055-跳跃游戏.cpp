7bda9d4079f43f9016faa3fe9dd28f601db822a5
qqwangxiaow <ywwangxiaow@qq.com>
2020-03-31 16:38:47 +0800

    greedy

---
+++ company/Bytedance/0055-跳跃游戏.cpp
@@ -0,0 +1,17 @@
+//greedy
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        if(nums.empty())
+            return false;
+        int curmax=nums[0];
+        for(int i=1;i<nums.size();i++)
+        {
+            if(curmax<i)
+                return false;
+            if(i+nums[i]>curmax)
+                curmax=i+nums[i];
+        }
+        return true;
+    }
+};
