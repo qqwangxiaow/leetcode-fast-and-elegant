6ffa3b2ee13ec3ce6f2cb9af6e19fb38b145d54d
qqwangxiaow <ywwangxiaow@qq.com>
2020-04-03 19:06:40 +0800

    binary search

---
+++ company/Bytedance/0240-搜索二维矩阵 II.cpp
@@ -0,0 +1,24 @@
+//col=n-1 
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if(matrix.empty())
+            return false;
+        int m=matrix.size(),n=matrix[0].size();
+        int row=0,col=n-1;
+        while(row<matrix.size()&&col>=0)
+        {
+            if(matrix[row][col]==target)
+                return true;
+            else if(matrix[row][col]<target)
+            {
+                row++;
+            }
+            else{
+                col--;
+            }
+            
+        }
+        return false;
+    }
+};
