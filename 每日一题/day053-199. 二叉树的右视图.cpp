89fbf5ef1759e4fbf88a3bf2790bf2b20b619658
qqwangxiaow <ywwangxiaow@qq.com>
2020-04-22 17:25:40 +0800

    levelorder

---
+++ 每日一题/day053-199. 二叉树的右视图.cpp
@@ -0,0 +1,29 @@
+/*
+level order
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        queue<TreeNode*>q;
+        vector<int>ret;
+        if(!root)
+            return ret;
+        q.push(root);
+        while(!q.empty())
+        {
+            int n=q.size();
+            for(int i=0;i<n;i++)
+            {
+                TreeNode *temp=q.front();
+                q.pop();
+                if(temp->left)
+                    q.push(temp->left);
+                if(temp->right)
+                    q.push(temp->right);
+                if(i==n-1)
+                    ret.push_back(temp->val);
+            } 
+        }
+        return ret;
+    }
+};
