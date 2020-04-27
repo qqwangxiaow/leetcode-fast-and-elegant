4b45018564cd76897328f388cd9707c7b3655931
qqwangxiaow <ywwangxiaow@qq.com>
2020-04-27 14:01:03 +0800

    dp

---
+++ summary/dp/0123-买卖股票的最佳时机 III.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if(prices.size()<2)
+            return 0;
+        int dp_0_0=0,dp_0_1=INT_MIN,dp_1_0=0,dp_1_1=INT_MIN;
+        for(int i=0;i<prices.size();i++)
+        {
+            dp_0_0=max(dp_0_0,dp_0_1+prices[i]);
+            dp_0_1=max(dp_0_1,dp_1_0-prices[i]);
+            dp_1_0=max(dp_1_0,dp_1_1+prices[i]);
+            dp_1_1=max(dp_1_1,-prices[i]);
+        }
+        return dp_0_0;
+    }
+};
