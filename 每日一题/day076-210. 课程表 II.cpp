ca83819083133f0489c194809d79216eb891f895
qqwangxiaow <ywwangxiaow@qq.com>
2020-05-17 13:58:08 +0800

    toposort

---
+++ 每日一题/day076-210. 课程表 II.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>>graph(numCourses,vector<int>());
+        vector<int>indegrees(numCourses,0);
+        for(auto prerequisite:prerequisites)
+        {
+            graph[prerequisite[1]].push_back(prerequisite[0]);
+            indegrees[prerequisite[0]]++;
+        }
+        queue<int>q;
+        vector<int>ret;
+        for(int i=0;i<numCourses;i++)
+        {
+            if(!indegrees[i])
+                q.push(i);
+        }
+        while(!q.empty())
+        {
+            int temp=q.front();
+            q.pop();
+            ret.push_back(temp);
+            for(auto each:graph[temp])
+            {
+                if(--indegrees[each]==0)
+                {
+                    q.push(each);
+                }
+            }
+        }
+        if(ret.size()==numCourses)
+            return ret;
+        else
+            return {};
+
+    }
+};
