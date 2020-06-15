df88098f91d86bb5171506f3248b4db0730c9c11
qqwangxiaow <ywwangxiaow@qq.com>
2020-06-15 23:05:35 +0800

    for

---
+++ 每日一题/day105-14. 最长公共前缀.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.empty())
+            return "";
+        if(strs.size()==1)
+            return strs[0];
+        for(int i=0;i<strs[0].size();i++)
+        {
+            int ch=strs[0][i];
+            for(auto str:strs)
+            {
+                if(str[i]!=ch)
+                {
+                    return strs[0].substr(0,i);
+                }
+            }
+        }
+        return strs[0];
+    }
+};
