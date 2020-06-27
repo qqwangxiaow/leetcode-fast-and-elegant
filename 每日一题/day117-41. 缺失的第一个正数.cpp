03416c2e7dc414eeae43f4e561094edbba5d839c
qqwangxiaow <ywwangxiaow@qq.com>
2020-06-27 11:44:46 +0800

    array

---
+++ 每日一题/day117-41. 缺失的第一个正数.cpp
@@ -0,0 +1,22 @@
+//while(nums[i]>0&&nums[i]<nums.size()&&nums[nums[i]-1]!=nums[i])
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]!=i+1)
+            {
+                while(nums[i]>0&&nums[i]<nums.size()&&nums[nums[i]-1]!=nums[i])
+                {
+                    swap(nums[nums[i]-1],nums[i]);
+                }
+            }
+        }
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]!=i+1)
+                return i+1;
+        }
+        return nums.size()+1;
+    }
+};
