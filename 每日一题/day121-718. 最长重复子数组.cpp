9a0a76c3399dc1180ec8186db4ca520a822f48f0
qqwangxiaow <ywwangxiaow@qq.com>
2020-07-01 22:28:02 +0800

    dp

--- 每日一题/day121-718. 最长重复子数组.cpp
+++ 每日一题/day121-718. 最长重复子数组.cpp
@@ -3,15 +3,18 @@ public:
     int findLength(vector<int>& A, vector<int>& B) {
         int m=A.size(),n=B.size();
         vector<vector<int>>dp(m+1,vector<int>(n+1,0));
-        for(int i=1;i<=A;i++)
+        int ret=0;
+        for(int i=1;i<=m;i++)
         {
-            for(int j=1;j<=B;j++)
+            for(int j=1;j<=n;j++)
             {
                 if(A[i-1]==B[j-1])
-                    dp[i][j]=dp[i-1][j-1];
-                else
-                    dp[i][j]=max(dp)
-            }   
+                {
+                    dp[i][j]=dp[i-1][j-1]+1;
+                    ret=max(dp[i][j],ret);
+                }            
+            }
         }
+        return ret;
     }
 };
