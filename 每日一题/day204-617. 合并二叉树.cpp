b713aa24829b7aceb64579a329d4b8a807e6ad25
qqwangxiaow <ywwangxiaow@qq.com>
2020-09-23 17:40:01 +0800

    recursive

---
+++ 每日一题/day204-617. 合并二叉树.cpp
@@ -0,0 +1,18 @@
+/**
+ dfs
+ */
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
+        if(!t1&&!t2)    
+            return nullptr;
+        if(!t1||!t2)
+            return t1?t1:t2;
+        TreeNode *root=new TreeNode(t1->val+t2->val);
+        root->left=mergeTrees(t1->left,t2->left);
+        root->right=mergeTrees(t1->right,t2->right);
+        return root;
+        
+
+    }
+};
