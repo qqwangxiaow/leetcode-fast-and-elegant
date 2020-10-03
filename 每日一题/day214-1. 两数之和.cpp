dfcabbecc946ceb5f151f56d0adc394ae09d4fd3
qqwangxiaow <ywwangxiaow@qq.com>
2020-10-03 21:51:30 +0800

    set

---
+++ 每日一题/day214-1. 两数之和.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int>mymap;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(mymap.find(target-nums[i])!=mymap.end())
+                return {i,mymap[target-nums[i]]};
+            else
+                mymap.emplace(nums[i],i);
+        }
+        return {};
+    }
+};
