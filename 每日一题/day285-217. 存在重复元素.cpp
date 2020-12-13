d7ecaa63806397b1dfb1937feb8f9601f78f489e
qqwangxiaow <ywwangxiaow@qq.com>
2020-12-13 23:14:39 +0800

    set

---
+++ 每日一题/day285-217. 存在重复元素.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        set<int>myset(nums.begin(),nums.end());
+        return !(myset.size()==nums.size());
+
+    }
+};
