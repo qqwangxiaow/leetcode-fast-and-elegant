313e65b343d3e9562921dba06ac890f197f8c34a
qqwangxiaow <ywwangxiaow@qq.com>
2020-12-30 00:17:03 +0800

    greedy

--- 每日一题/day301-330. 按要求补齐数组.cpp
+++ 每日一题/day301-330. 按要求补齐数组.cpp
@@ -1,18 +1,23 @@
+//total当前最大能表示的数，遇到nums[i]后两种情况
+//[1,total]U[1+nums[k],total+nums[k]]U[nums[k],nums[k]]
+//一种情况是无缝隙，直接累加nums[i]，一种有缝隙，贪心添加total+1
 class Solution {
 public:
     int minPatches(vector<int>& nums, int n) {
-        int patches = 0;
-        long long x = 1;
-        int length = nums.size(), index = 0;
-        while (x <= n) {
-            if (index < length && nums[index] <= x) {
-                x += nums[index];
-                index++;
-            } else {
-                x <<= 1;
-                patches++;
+        long total = 0;
+        int ret = 0;
+        int index = 0;
+        while(total < n){
+            if(index < nums.size() && nums[index] <= total+1){
+                total += nums[index++];
+            }
+            else{
+                total = total + (total + 1);
+                ret++;
             }
         }
-        return patches;
+        return ret;
     }
 };
+
+
