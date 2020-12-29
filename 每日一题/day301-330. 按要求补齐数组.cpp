e011cbb47bd990f83f734cfa7dc9b4a547b38b61
qqwangxiaow <ywwangxiaow@qq.com>
2020-12-29 23:56:01 +0800

    greedy

---
+++ 每日一题/day301-330. 按要求补齐数组.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minPatches(vector<int>& nums, int n) {
+        int patches = 0;
+        long long x = 1;
+        int length = nums.size(), index = 0;
+        while (x <= n) {
+            if (index < length && nums[index] <= x) {
+                x += nums[index];
+                index++;
+            } else {
+                x <<= 1;
+                patches++;
+            }
+        }
+        return patches;
+    }
+};
