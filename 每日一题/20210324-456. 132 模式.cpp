94a68f657d2fa742dac20102467b0a1e1b0d3802
autumoon <ywwangxiaow@qq.com>
2021-03-24 23:43:52 +0800

    stack

---
+++ 每日一题/20210324-456. 132 模式.cpp
@@ -0,0 +1,18 @@
+//维护一个单调递减的栈，栈里面的数一定大于k
+class Solution {
+public:
+    bool find132pattern(vector<int>& nums) {
+        stack<int>stk;
+        int k = INT_MIN;
+        for(int i = nums.size() - 1; i >= 0; --i){
+            if(nums[i] < k)
+                return true;
+            while(!stk.empty() && stk.top() < nums[i]){
+                k = max(stk.top(), k);
+                stk.pop();
+            }
+            stk.push(nums[i]);
+        }
+        return false;
+    }
+};
